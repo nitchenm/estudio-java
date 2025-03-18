b7e15e6e736f27cb68e7e381fea993a5610d411c
nitchenm <nitchenm@gmail.com>
2025-03-18 18:21:19 -0300

    Adding weapon equipment

--- src/main/java/com/duoc/Main.java
+++ src/main/java/com/duoc/Main.java
@@ -3,5 +3,17 @@ package com.duoc;
 public class Main {
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
+
+        MainCharacter playerOne = new MainCharacter(100, "Warrior", inventory);
+        Weapon axe = new Weapon("Axe", 1, 10, "Steel axe");
+
+        Npc beast = new Npc(50);
+
+        playerOne.equipWeapon(axe);
+        beast.doDamage(10, playerOne);
+        playerOne.doDamage(20, beast);
+
+        System.out.println(beast.getCurrentHealth());
+        System.out.println(playerOne.getCurrentHealth());
     }
 }
--- src/main/java/com/duoc/MainCharacter.java
+++ src/main/java/com/duoc/MainCharacter.java
@@ -4,18 +4,31 @@ public class MainCharacter extends LivingEntity{
     
     private String name ;
     private Inventory inventory;
+    private Weapon equippedWeapon;
+    private Weapon fist;
     
 
     public MainCharacter(int maxHealth, String name, Inventory inventory) {
         super(maxHealth);
         this.name = name;
         this.inventory = inventory;
+        this.fist = new Weapon("Fist", 1, 1, "Fist weapon");
+    }
+
+    public void equipWeapon(Weapon weaponToEquip){
+        this.equippedWeapon = weaponToEquip;
     }
 
     
     @Override
     public void doDamage(int damage, LivingEntity enemyliLivingEntity) {
-        enemyliLivingEntity.setCurrentHealth(damage);
-    }
+        if(this.equippedWeapon != null){
+            enemyliLivingEntity.setCurrentHealth(damage + this.equippedWeapon.getDamage());
+        }
+        else{
+            equipWeapon(this.fist);
+            enemyliLivingEntity.setCurrentHealth(damage + this.equippedWeapon.getDamage());
+        }
+    }   
     
 }
